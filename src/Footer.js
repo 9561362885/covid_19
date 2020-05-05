d9bbbccc4042537718f4292a6cf0b7acc71954b1
9561362885 <patilashishdx@gmail.com>
2020-05-05 08:57:24 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './Footer.module.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <h2>This project is developed by ASHISH PATIL</h2>
+            </div>
+        )
+    }
+}
+
+export default Footer
